a075ae519a9a080073f4046b96b493c16781b19d
chandhu <chandhu.mpc19@gmail.com>
2022-10-26 23:49:31 +0530

    added first two schematypes

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,49 @@
 const { 
     GraphQLObjectType,
-    GraphQLInt,
-    GraphQLList } =require("graphql")
+    GraphQLList, 
+    GraphQLSchema,
+    GraphQLID,
+    GraphQLString,
+    GraphQLInt} =require("graphql")
+const {axios}=require("axios")
 
+  
+const userType=new GraphQLObjectType({
+    name:"User",
+    fields:()=>({
+        id : { type : GraphQLID },
+        name : { type : GraphQLString },
+        email : { type : GraphQLString },
+        phone : { type : GraphQLString},
+        address : { type : GraphQLString},
+        website : { type : GraphQLString},
+        company : { type : GraphQLString}
+
+
+
+    })
+})    
+const rootQuery={
+    name:"query",
+    fields:{
+        user:{
+        type:userType,
+        args:{id : { type:GraphQLID } },
+        resolve: async(parent,args)=>{
+            const userList=await axios.get("https://jsonplaceholder.typicode.com/users")
+            return userList.find(user=>user.id===args.id)
+        }
+        
+        },
+        users:{
+            type:userType,
+            args:{id : { type:GraphQLID } },
+            resolve: async(parent,args)=>{
+                const userList=await axios.get("https://jsonplaceholder.typicode.com/users")
+                return userList
+            }
+        }
+ }
+}
+
+module.exports=new GraphQLSchema(rootQuery)
